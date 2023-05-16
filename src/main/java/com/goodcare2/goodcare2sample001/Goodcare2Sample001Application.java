0339a936efc6de5035443b539be62e2775319974
Chris <44944612+chris910512@users.noreply.github.com>
2023-05-16 10:51:48 +0900

    Initial commit

---
+++ src/main/java/com/goodcare2/goodcare2sample001/Goodcare2Sample001Application.java
@@ -0,0 +1,13 @@
+package com.goodcare2.goodcare2sample001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Goodcare2Sample001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Goodcare2Sample001Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/goodcare2/goodcare2sample001/Goodcare2Sample001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodcare2.goodcare2sample001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Goodcare2Sample001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
